c5655576088ab2aba794003e48b0c4a62ee94b9b
Krzysztof Bieron <krzysztof.bieron@gmail.com>
2016-10-08 09:52:42 +0200

    First Hotel class interface

---
+++ src/projobiekt_hotel/Hotel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projobiekt_hotel;
+
+import java.util.*;
+/**
+ *
+ * @author Bieron
+ */
+public class Hotel {
+    
+    public void ReserveRoom() {
+        
+    }
+    
+    public void CancelRoomReservation() {
+        
+    }
+    
+    public void ExtendRoomReservation() {
+        
+    }
+    
+    public void AddRoom() {
+        
+    }
+    
+    public void AddEmployee() {
+        
+    }
+    
+    public void Login(String email,String password) {
+        
+    }
+    
+    
+    
+    private  ArrayList<Room> rooms;
+    private ArrayList<Employee> employees;
+}
