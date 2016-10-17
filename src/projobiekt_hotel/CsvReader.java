f20257dcf697b3f44a02a8c0b786968891ab4c16
Maciej Michalec <michalec@spk-ssh-access1.if.uj.edu.pl>
2016-10-17 12:50:29 +0200

    Implemented reading Rooms info from CSV files

--- src/projobiekt_hotel/CsvReader.java
+++ src/projobiekt_hotel/CsvReader.java
@@ -60,5 +60,53 @@ public class CsvReader {
 		
 		return new User(mailAddress, passwordHash, name, userRole);
 	}
+	
+	public ArrayList<Room> readRoomsList(String filename){
+		ArrayList<Room> rooms = new ArrayList<Room>();
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(filename));
+			
+			String line;
+			while( (line = reader.readLine()) != null) {
+				try {
+					Room newRoom = readRoomLine(line);
+					rooms.add(newRoom);
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return rooms;
+	}
+	
+	public Room readRoomLine(String line) throws Exception
+	{
+		String[] elements = line.split(separator);
+		if(elements.length != 6){
+			throw new Exception("wrong line in csv file");
+		}
+		
+		int roomNumber = Integer.parseInt(elements[0]);
+		int maxPeople = Integer.parseInt(elements[1]);
+		int price = Integer.parseInt(elements[2]);
+		boolean hasBath = Boolean.parseBoolean(elements[3]);
+		boolean hasKitchen = Boolean.parseBoolean(elements[4]);
+		boolean hasGoodWindowView = Boolean.parseBoolean(elements[5]);
+		
+		return new Room(roomNumber, maxPeople, price, hasBath, hasKitchen, hasGoodWindowView);
+	}
 
 }
--- src/tests/CsvReaderTests.java
+++ src/tests/CsvReaderTests.java
@@ -47,11 +47,61 @@ public class CsvReaderTests {
 		String wrongLine = "foo@bar.pl,employee";
 		
 		try {
-		reader.readUserLine(wrongLine);
-		fail("Exception not thrown");
+			reader.readUserLine(wrongLine);
+			fail("Exception not thrown");
 		} catch(Exception e) {
 			assertEquals("wrong line in csv file", e.getMessage());
 		}
 	}	
+	
+	@Test
+	public void testReadRoomsList() {
+		String testFile = new File(this.getClass().getResource("/tests/resources/rooms.csv").getFile()).getAbsolutePath();
+		CsvReader reader = new CsvReader();
+		ArrayList<Room> rooms = reader.readRoomsList(testFile);
+		
+		assertEquals(4, rooms.size());
+		
+		Room firstRoom = rooms.get(0);
+		Room secondRoom = rooms.get(1);
+		Room thirdRoom = rooms.get(2);
+		Room fourthRoom = rooms.get(3);
+		
+		assertEquals(5, firstRoom.number());
+		assertEquals(120, firstRoom.price());
+		assertEquals(2, secondRoom.people());
+		assertEquals(true, secondRoom.hasBath());
+		assertEquals(false, secondRoom.hasKitchen());
+		assertEquals(3, thirdRoom.people());
+		assertEquals(240, fourthRoom.price());
+		assertEquals(true, fourthRoom.hasKitchen());
+	}
+	
+	@Test
+	public void testReadRoomLine() throws Exception {
+		CsvReader reader = new CsvReader();
+		String line = "3,2,135,true,false,true";
+		Room room = reader.readRoomLine(line);
+		
+		assertEquals(3, room.number());
+		assertEquals(2, room.people());
+		assertEquals(135, room.price());
+		assertEquals(true, room.hasBath());
+		assertEquals(false, room.hasKitchen());
+		assertEquals(true, room.hasGoodWindowView());
+	}
+	
+	@Test
+	public void testMalformedRoomLine() {
+		CsvReader reader = new CsvReader();
+		String wrongLine = "3,2,true,false,true";
+		try {
+			reader.readRoomLine(wrongLine);
+			fail("Exception not thrown");
+		} catch(Exception e) {
+			assertEquals("wrong line in csv file", e.getMessage());
+		}
+				
+	}
 
 }
