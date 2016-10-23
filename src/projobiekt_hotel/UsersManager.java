623865f3511b9e8a7d26182e1bfe87a4910514bd
Maciej Michalec <maciej.michalec@gmail.com>
2016-10-23 15:55:32 +0200

    Added UsersManager

---
+++ src/projobiekt_hotel/UsersManager.java
@@ -0,0 +1,69 @@
+package projobiekt_hotel;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+
+public class UsersManager {
+
+	public static boolean login(String mail, String password) {
+		try {
+			User user = getUserByMail(mail);
+			if(user.passwordHash().equals(hash(password)))
+				return true;
+			else
+				return false;
+		} catch(Exception e) {
+			return false;
+		}
+	}
+	
+	public static boolean register(String mail, String password, String name, String role) throws Exception {
+		try {
+			getUserByMail(mail);
+			System.out.println("Cannot register - e-mail exists in database");
+			return false;
+		} catch(Exception e) {
+			System.out.println("Registering new user");
+		}
+		
+		ListsCloud cloud = ListsCloud.getInstance();
+		ArrayList<User> users = cloud.users();
+		User newUser = new User(mail, hash(password), name, UserRole.fromString(role)); 
+		users.add(newUser);
+		
+		return true;
+	}
+	
+	public static boolean correctSecretPassword(String password) throws Exception {
+		return hash(password).equals("e919c49d5f0cd737285367810a3394d0");
+	}
+	
+
+	public static User getUserByMail(String mail) throws Exception {
+		ListsCloud cloud = ListsCloud.getInstance();
+		ArrayList<User> users = cloud.users();
+		for (User user : users) {
+			if(user.mailAddress() == mail)
+				return user;
+		}
+		throw new Exception("user not found");
+	}
+	
+	
+	public static String hash(String plain) throws Exception {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.reset();
+		md.update(plain.getBytes());
+		byte[] digest = md.digest();
+		BigInteger bigInt = new BigInteger(1, digest);
+		String hashedPassword = bigInt.toString(16);
+		
+		while(hashedPassword.length() < 32 ){
+			hashedPassword = "0"+hashedPassword;
+		}
+		
+		return hashedPassword;
+	}
+	
+}
---
+++ src/tests/UsersManagerTests.java
@@ -0,0 +1,78 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import projobiekt_hotel.ListsCloud;
+import projobiekt_hotel.User;
+import projobiekt_hotel.UserRole;
+import projobiekt_hotel.UsersManager;
+
+public class UsersManagerTests {
+
+	@Test
+	public void testHash() throws Exception {
+		assertEquals("3858f62230ac3c915f300c664312c63f", UsersManager.hash("foobar"));
+		assertEquals("c06db68e819be6ec3d26c6038d8e8d1f", UsersManager.hash("test12345"));
+		assertEquals("d8578edf8458ce06fbc5bb76a58c5ca4", UsersManager.hash("qwerty"));
+		assertEquals("1d64dce239c4437b7736041db089e1b9", UsersManager.hash("abcdefghijklmnop"));
+	}
+	
+	@Test
+	public void testGetUserByMail(){
+		ListsCloud cloud = ListsCloud.getInstance();
+		ArrayList<User> users = cloud.users();
+		users.clear();
+		User newUser = new User("foo@bar.com", "d8578edf8458ce06fbc5bb76a58c5ca4", "Jan Kowalski", UserRole.EMPLOYEE);
+		users.add(newUser);
+		
+		try {
+			assertEquals(newUser, UsersManager.getUserByMail("foo@bar.com"));
+		} catch(Exception e) {
+			fail("Exception thrown");
+		}
+		
+		try {
+			User someUser = UsersManager.getUserByMail("a@b.com");
+			fail("Exception not thrown");
+		} catch(Exception e) {
+			assertEquals("user not found", e.getMessage());
+		}
+		
+	}
+	
+	@Test
+	public void testLogin() throws Exception {
+		ListsCloud cloud = ListsCloud.getInstance();
+		ArrayList<User> users = cloud.users();
+		users.clear();
+		
+		String plainPassword = "myPass";
+		String hashedPassword = UsersManager.hash(plainPassword);
+		
+		User newUser = new User("foo@bar.com", hashedPassword, "Jan Kowalski", UserRole.EMPLOYEE);
+		users.add(newUser);
+		
+		assertEquals(false, UsersManager.login("wrong@user.com", plainPassword));
+		assertEquals(true, UsersManager.login("foo@bar.com", plainPassword));
+		assertEquals(false, UsersManager.login("foo@bar.com", "myPas"));
+	}
+	
+	@Test
+	public void testRegister() throws Exception {
+		ListsCloud cloud = ListsCloud.getInstance();
+		ArrayList<User> users = cloud.users();
+		users.clear();
+		
+		assertEquals(false, UsersManager.login("foo@bar.com", "myPass"));
+		assertEquals(true, UsersManager.register("foo@bar.com", "myPass", "Jan Kowalski", "guest"));
+		assertEquals(true, UsersManager.login("foo@bar.com", "myPass"));
+		assertEquals(false, UsersManager.register("foo@bar.com", "otherPass", "Jan Kowalski", "guest"));
+	}
+
+}
